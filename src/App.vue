723ac07d6ac75a0ae3852ca929cac5ba8b816ee7
Pedro Paulo Silva <98651810+p3dr1n80@users.noreply.github.com>
2024-05-28 09:56:13 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="js">
+import Banner from '@/components/Banner.vue'
+import MainContent from "@/components/MainContent.vue";
+
+export default {
+  components: {MainContent, Banner}
+}
+</script>
+
+<template>
+  <Banner/>
+  <MainContent/>
+</template>
+
+<style scoped>
+
+</style>
